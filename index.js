33d7e1f2e2a85f0c83bddb106b6d1744a2b4222c
Keval Thakarar <thakararkeval1010@gmail.com>
2022-02-17 20:35:33 +0530

    Add files

--- index.js
+++ index.js
@@ -2,15 +2,39 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const yup = require('yup');
+const {nanoid} = require('nanoid');
+const monk = require('monk');
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+urls.createIndex({slug : 1} , {unique : true});
 
 const port = process.env.PORT || 3000;
 const app = express();
+app.enable('trust proxy');
 
-app.use(helmet());
+// app.use(
+//     helmet({
+//         contentSecurityPolicy: true,
+//     })
+// );
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
+app.use((error , req , res , next) => {
+    if(error.status)
+        res.status(error.status);
+    else 
+        res.status(501);
+
+    res.json({
+        message : error.message,
+        stack : process.env.NODE_ENV === 'production'? 'production' : error.stack
+    })
+})
 
 
 // app.get('/url/:id' , (req , res) => {
@@ -18,14 +42,63 @@ app.use(express.static('./public'));
 
 // });
 
-// app.get('/:id' , (req , res) => {
-//     // #TODO : redirect to url
+app.get('/i/:id' , async (req , res , next) => {
+    const {id : slug} = req.params;
+    try{
+        const url = await urls.findOne({slug});
+        console.log(url);
+        if(url){
+            console.log(url.url);
+            res.redirect(url.url);
+            return;
+        } 
+        res.redirect(`?error={slug} not found`);
+    }catch(error){
+        res.redirect(`?error=Link not found`);
+    }
 
-// })
+});
 
-// app.get('/url' , (req , res) => {
-//     // #TODO : create a short URL
-// })
+const schema = yup.object().shape({
+    slug : yup.string().trim().matches(/[\w\-]/i),
+    url : yup.string().trim().url().required()
+});
+
+app.post('/url' , async (req , res ,next) => {
+    let {url , slug} = req.body;
+    console.log(url);
+    console.log(slug);
+    try{
+        await schema.validate({
+            slug,
+            url
+        });
+
+        if(!slug){
+            slug = nanoid(5);
+        }
+        else{
+            const existing = await urls.findOne({slug});
+            if(existing)
+                throw new Error('SLUG is USED');
+        }
+        slug = slug.toLowerCase();
+        const secret = nanoid(10).toLowerCase();
+
+        const new_url = {
+            url,
+            slug,
+        };
+
+        const created = await urls.insert(new_url);
+        console.log(created);
+        res.send(created);
+    }catch(error){
+        next(error);
+    }
+})
+
+
 
 app.get('/' , (req , res) => {
     res.json({
